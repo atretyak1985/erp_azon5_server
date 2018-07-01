c9af02931b1f41e649766c2a1dad0c18fb84d152
tretyaka <atretyak1985@gmail.com>
2018-07-01 21:44:54 +0300

    fixed config

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@ from app import app as application
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app.database import db
+from app import app as application
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('app/config.ini')
 
 manager = Manager(application)
 migrate = Migrate(application, db, 'migrations')
@@ -15,5 +16,6 @@ import logging
 logging.basicConfig()
 
 if __name__ == '__main__':
-    manager.run()
-    #app.run(port=config.getint('DEFAULT', 'APP_PORT'), debug=config['DEFAULT'].getboolean('APP_DEBUG'))
+    #manager.run()
+    #application.run()
+    application.run(port=config.getint('DEFAULT', 'APP_PORT'), debug=True)
